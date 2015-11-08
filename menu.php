e15047422ef13603f33a009c35325e58cdc9c272
Fallen Angel <mr.x97@mail.ru>
2015-11-08 18:18:15 +0200

    Some new transparent themes

--- menu.php
+++ menu.php
@@ -16,41 +16,48 @@
 <!-- MENU. END -->
 <div id="Content">
         <div id="leftSideBar">
-            <span>&lt;p&gt; Параграф</span><br>
-            <span>&lt;table&gt; Таблица</span><br>
-            <span>&lt;img&gt; Изображение</span><br>
-            <span>&lt;div&gt; Блок div</span><br>
-            <span>&lt;span&gt; Блок span</span><br>
-            <span>&lt;input type="text"&gt;</span><br>
-            <span>&lt;input type="password"&gt;</span><br>            
-            <span>&lt;textarea&gt;</span><br>
-            <select>
-                <option value="" disabled selected>
-                    &lt;hX&gt;
-                 </option>
-                <option>
-                    &lt;h1&gt;
-                </option>
-                <option>
-                    &lt;h2&gt;
-                </option>
-                <option>
-                    &lt;h3&gt;
-                </option>                
-                <option>
-                    &lt;h4&gt;
-                </option>
-                <option>
-                    &lt;h5&gt;
-                </option>
-                <option>
-                    &lt;h6&gt;
-                </option>
-            </select>
+            <div id="leftSideWrap">
+                <div id="elementsList">
+                    <span>&lt;p&gt; Параграф</span><br>
+                    <span>&lt;table&gt; Таблица</span><br>
+                    <span>&lt;img&gt; Изображение</span><br>
+                    <span>&lt;div&gt; Блок div</span><br>
+                    <span>&lt;span&gt; Блок span</span><br>
+                    <span>&lt;input type="text"&gt;</span><br>
+                    <span>&lt;input type="password"&gt;</span><br>            
+                    <span>&lt;textarea&gt;</span><br>
+                    <select>
+                        <option value="" disabled selected>
+                            &lt;hX&gt;
+                         </option>
+                        <option>
+                            &lt;h1&gt;
+                        </option>
+                        <option>
+                            &lt;h2&gt;
+                        </option>
+                        <option>
+                            &lt;h3&gt;
+                        </option>                
+                        <option>
+                            &lt;h4&gt;
+                        </option>
+                        <option>
+                            &lt;h5&gt;
+                        </option>
+                        <option>
+                            &lt;h6&gt;
+                        </option>
+                    </select>
+                </div>
+                <div id="leftBar">
+                    <div id="wrapEl"><div style="display: table-cell;vertical-align: middle; text-align: center">Элементы</div></div>
+                </div>
+            </div>
         </div>
          <div id="pages">
              <div id="pagesWrapper">
-                 <div id="pagesBanner">
+                 <div class="pagesBanner">
                      СТРАНИЦЫ
                  </div>
                  <div id="pagesLinks">
