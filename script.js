262787cae214bc8ba6b648a8870363a9c3d9096e
Fallen Angel <mr.x97@mail.ru>
2015-11-08 12:34:24 +0200

    custom context menu

--- script.js
+++ script.js
@@ -1,6 +1,63 @@
 $(document).ready(function () {
+    $("html").click(function () {
+        var elem = document.getElementsByClassName("ownContextMenu")[0];
+        document.getElementsByTagName("body")[0].removeChild(elem);
+    });
+    if (document.addEventListener) {
+        document.addEventListener('contextmenu', function (e) {
+            if ($(e.target).is(".dynamicElement")) {
+                var node = document.createElement("div");
+                node.innerHTML = "<span>Refresh</span><br><span>Change class</span>";
+                node.className = "ownContextMenu";
+                node.style.left = e.clientX + "px";
+                node.style.top = e.clientY + "px";
+                document.getElementsByTagName("body")[0].appendChild(node);
+                e.preventDefault();
+            }
+        }, false);
+
+    } else {
+        document.attachEvent('oncontextmenu', function () {
+            alert("You've tried to open context menu");
+            window.event.returnValue = false;
+        });
+    }
+
+
     $("#leftSideBar span").click(function () {
-        $("#pageHolder").append("<div class='sampleBlock'>DIV</div>")
+        var text = this.innerHTML.substring(4, this.innerHTML.indexOf("&gt;"));
+        if (text.indexOf("\"") <= 0)
+            node = document.createElement(text);
+        switch (text) {
+            case "p":
+                node.className = "sampleParagraph dynamicElement";
+                node.innerHTML = "paragraph";
+                break;
+            case "img":
+                node.className = "dynamicElement";
+                node.src = "sampleImg.jpg";
+                break;
+            case "div":
+                node.className = "sampleBlock";
+                node.className = "dynamicElement";
+                node.innerHTML = "DIV";
+                break;
+            case "span":
+                node.innerHTML = "span";
+                node.className = "dynamicElement";
+                break;
+            case "input type=\"text\"":
+                node.className = "dynamicElement";
+                node = document.createElement("input");
+                break;
+            case "input type=\"password\"":
+                node = document.createElement("input");
+                node.className = "dynamicElement";
+                node.type = "password";
+                break;
+        }
+        var myNode = document.getElementById("pageHolder");
+        myNode.appendChild(node);
     });
     $('#code').click(function () {
 
